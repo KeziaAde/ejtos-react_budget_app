ee744a657981be9d906b055e9fbb33b1d6adc9af
KeziaAde <k.adae304@gmail.com>
2024-02-06 20:39:12 -0500

    last proj for module

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,16 +2,28 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { dispatch, budget } = useContext(AppContext);
+    const { expenses } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
     const handleBudgetChange = (event) => {
         setNewBudget(event.target.value);
+        if (expenses <= newBudget <= 20000) {
+            dispatch({
+                type: 'SET_BUDGET',
+                payload: newBudget,
+            });
+        } else if (newBudget > 20000) {
+            window.alert("The budget cannot exceed £20,000")
+        } else if (newBudget < expenses) {
+            window.alert("You cannot reduce the budget lower than the spending")
+        }
     }
+
     return (
-<div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
-<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
-</div>
+        <div className='alert alert-secondary'>
+            <span>Budget: £{budget}</span>
+            <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
+        </div>
     );
 };
 export default Budget;
--- src/components/ExpenseTotal.js
+++ src/components/ExpenseTotal.js
@@ -1,10 +1,14 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 const ExpenseTotal = () => {
-    const { expenses } = useContext(AppContext);
+    const { dispatch, expenses } = useContext(AppContext);
     const totalExpenses = expenses.reduce((total, item) => {
         return (total += item.cost);
     }, 0);
+    dispatch({
+        type: 'RED_EXPENSE',
+        payload: totalExpenses,
+    });
     return (
         <div className='alert alert-primary'>
             <span>Spent so far: £{totalExpenses}</span>
